df29dfb1a78b96c6036da1ea2fcd31cda6500279
Frajo Haider <f_haider@gmx.at>
2016-04-11 20:32:55 +0200

    implement app closing
    Change-Id: I13f4550799803be6d179d2c14d758c214ed34a55

--- services/java/sfdroid/Helpers.java
+++ services/java/sfdroid/Helpers.java
@@ -3,5 +3,6 @@ package sfdroid;
 public class Helpers
 {
     public static native void notify_of_app_start(String component);
+    public static native void notify_of_app_close(String component);
 };
 
