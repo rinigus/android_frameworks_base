259687525912e41b5fda8a8bc0d935869085b61b
Frajo Haider <f_haider@gmx.at>
2016-04-15 21:45:52 +0200

    disable app transitions
    Change-Id: I4e04a5483477997ef94d45e6813597e47a4408bc

--- services/core/java/com/android/server/wm/AppTransition.java
+++ services/core/java/com/android/server/wm/AppTransition.java
@@ -947,7 +947,8 @@ public class AppTransition implements Dump {
                     + " transit=" + transit + " isEntrance=" + enter
                     + " Callers=" + Debug.getCallers(3));
         }
-        return a;
+        return null;
+        //return a;
     }
 
     void postAnimationCallback() {
