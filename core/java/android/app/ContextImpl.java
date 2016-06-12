6e845afa85b98236a3974a43b182affe5dda1245
Frajo Haider <f_haider@gmx.at>
2016-06-12 12:10:06 +0200

    [sfdroid]: disable NFC
    Change-Id: I4d5f2360841e06667d29e32020864e2fece1710d

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -440,10 +440,12 @@ class ContextImpl extends Context {
                     return new BatteryManager(service);
                 }});
 
+/*
         registerService(NFC_SERVICE, new ServiceFetcher() {
                 public Object createService(ContextImpl ctx) {
                     return new NfcManager(ctx);
                 }});
+*/
 
         registerService(DROPBOX_SERVICE, new StaticServiceFetcher() {
                 public Object createStaticService() {
